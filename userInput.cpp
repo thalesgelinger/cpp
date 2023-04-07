082ba5e3c917d1c80e319b3dec96be58d562b1a5
Thales Augusto Gelinger <thalesgelinger@Thaless-MacBook-Air.local>
2023-04-07 20:15:37 -0300

    user input

---
+++ userInput.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+
+using namespace std;
+
+int main (){
+    int age;
+    cout << "Type your age: "<< endl;
+    cin >> age;
+
+    cout << "Your age is: " << age << endl;
+
+    return 0;
+}
